8d1a5eb227913317c2c4eea0687e9bedaec722b6
sailorlqh <qinhang@google.com>
2024-12-02 23:23:44 +0000

    feat: add GoogleGroupsToGithubMapper and support for creating google groups identity clients

---
+++ pkg/client.go/mapper.go
@@ -0,0 +1,88 @@
+// Copyright 2024 The Authors (see AUTHORS file)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package client
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"google.golang.org/protobuf/encoding/prototext"
+
+	"github.com/abcxyz/team-link/apis/v1alpha3"
+	tltypes "github.com/abcxyz/team-link/internal"
+	"github.com/abcxyz/team-link/pkg/github"
+	"github.com/abcxyz/team-link/pkg/groupsync"
+)
+
+type GoogleGroupToGitHubMapper struct {
+	GoogleGroupToGitHubTeam map[string][]string
+}
+
+// AllGroupIDs returns the set of groupIDs being mapped (the key set).
+func (g *GoogleGroupToGitHubMapper) AllGroupIDs(ctx context.Context) ([]string, error) {
+	res := make([]string, 0)
+	for key := range g.GoogleGroupToGitHubTeam {
+		res = append(res, key)
+	}
+	return res, nil
+}
+
+// ContainsGroupID returns whether this mapper contains a mapping for the given group ID.
+func (g *GoogleGroupToGitHubMapper) ContainsGroupID(ctx context.Context, groupID string) (bool, error) {
+	_, ok := g.GoogleGroupToGitHubTeam[groupID]
+	return ok, nil
+}
+
+// MappedGroupIDs returns the list of group IDs mapped to the given group ID.
+func (g *GoogleGroupToGitHubMapper) MappedGroupIDs(ctx context.Context, groupID string) ([]string, error) {
+	githubTeamIDs, ok := g.GoogleGroupToGitHubTeam[groupID]
+	if !ok {
+		return nil, fmt.Errorf("no mapping found for group ID: %s", groupID)
+	}
+	return githubTeamIDs, nil
+}
+
+// newGoogleGroupToGitHubMapper creates a GoogleGroupToGitHubMapper using the provided mapping file
+func newGoogleGroupToGitHubMapper(groupMappingFile string) (groupsync.OneToManyGroupMapper, error) {
+	b, err := os.ReadFile(groupMappingFile)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read mapping file: %w", err)
+	}
+	tm := &v1alpha3.GoogleGroupToGitHubTeamMappings{}
+	if err := prototext.Unmarshal(b, tm); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal mapping file: %w", err)
+	}
+	mappings := make(map[string][]string)
+	for _, v := range tm.GetMappings() {
+		if _, ok := mappings[*v.GoogleGroup.GroupId]; !ok {
+			mappings[*v.GoogleGroup.GroupId] = []string{github.Encode(*v.GitHubTeam.OrgId, *v.GitHubTeam.TeamId)}
+		} else {
+			mappings[*v.GoogleGroup.GroupId] = append(mappings[*v.GoogleGroup.GroupId], github.Encode(*v.GitHubTeam.OrgId, *v.GitHubTeam.TeamId))
+		}
+	}
+	return &GoogleGroupToGitHubMapper{
+		GoogleGroupToGitHubTeam: mappings,
+	}, nil
+}
+
+// NewOneToManyGroupMapper creates a groupsync.OneToManyMapper base on the input source
+// and destination system type using provided groupMappingFile
+func NewOneToManyGroupMapper(source, dest, groupMappingFile string) (groupsync.OneToManyGroupMapper, error) {
+	if source == tltypes.SystemTypeGoogleGroups && dest == tltypes.SystemTypeGitHub {
+		return newGoogleGroupToGitHubMapper(groupMappingFile)
+	}
+	return nil, fmt.Errorf("unsupported source to dest mapper type: source %s, dest %s", source, dest)
+}
---
+++ pkg/client.go/mapper_test.go
@@ -0,0 +1,112 @@
+package client
+
+import (
+	"os"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"google.golang.org/protobuf/testing/protocmp"
+
+	"github.com/abcxyz/pkg/testutil"
+	tltypes "github.com/abcxyz/team-link/internal"
+)
+
+func TestCreateGoogleGroupToGithubMapper(t *testing.T) {
+	t.Parallel()
+	defaultWritePath := "test.textproto"
+	cases := []struct {
+		name                          string
+		fileReadpath                  string
+		content                       string
+		wantGoogleGroupToGitHubMapper *GoogleGroupToGitHubMapper
+		wantErr                       string
+	}{
+		{
+			name: "success",
+			content: `
+mappings: [
+  {
+    google_group: {
+      group_id: "test_id_1"
+    }
+    git_hub_team: {
+      org_id: 1
+      team_id: 2
+    }
+  },
+  {
+    google_group: {
+      group_id: "test_id_1"
+    }
+    git_hub_team: {
+      org_id: 1
+      team_id: 3
+    }
+  },
+  {
+    google_group: {
+      group_id: "test_id_2"
+    }
+    git_hub_team: {
+      org_id: 1
+      team_id: 4
+    }
+  }
+]
+`,
+			wantGoogleGroupToGitHubMapper: &GoogleGroupToGitHubMapper{
+				GoogleGroupToGitHubTeam: map[string][]string{
+					"test_id_1": {"1:2", "1:3"},
+					"test_id_2": {"1:4"},
+				},
+			},
+		},
+		{
+			name:         "file_not_exist",
+			fileReadpath: "not_exist_path",
+			wantErr:      "failed to read mapping file",
+		},
+		{
+			name:    "invalid_format",
+			content: `not valid`,
+			wantErr: "failed to unmarshal mapping file",
+		},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			// ctx := context.Background()
+			td := t.TempDir()
+
+			// Create a file in the temporary directory
+			tempFile, err := os.CreateTemp(td, defaultWritePath)
+			if err != nil {
+				t.Fatal("failed to create tempFile: %w", err)
+			}
+			defer os.Remove(tempFile.Name())
+
+			// Write some data to the file
+			_, err = tempFile.WriteString(tc.content)
+			if err != nil {
+				t.Fatal("failed to write tempFile: %w", err)
+			}
+
+			if tc.fileReadpath == "" {
+				tc.fileReadpath = tempFile.Name()
+			}
+			res, err := NewOneToManyGroupMapper(tltypes.SystemTypeGoogleGroups, tltypes.SystemTypeGitHub, tc.fileReadpath)
+			if diff := testutil.DiffErrString(err, tc.wantErr); diff != "" {
+				t.Errorf("unexpected err: %s", diff)
+			}
+			if err != nil {
+				return
+			}
+			if diff := cmp.Diff(res, tc.wantGoogleGroupToGitHubMapper, protocmp.Transform()); diff != "" {
+				t.Errorf("got unexpected response:\n%s", diff)
+			}
+		})
+	}
+}
